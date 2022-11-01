9fac96a912679157f63dd48b4bf1c4f0d004a1f9
Zane <zhawken@gmail.com>
2022-11-01 21:02:31 +1300

    Transfered user context and categories context into React-Redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+
+import { onAuthStateChangedListner, createUserDocumentFromAuth, getCategoriesAndDocuments } from './utils/firebase/firebase.utils';
+import { setCurrentUser } from "./store/user/user.action"
+import { setCategoriesMap } from "./store/categories/category.action"
 
 import Authentication from './routes/authentication/authentication.component';
 import Checkout from './routes/checkout/checkout.component';
@@ -8,19 +13,42 @@ import Navigation from './routes/navigation/navigation.component';
 import Shop from './routes/shop/shop.component';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListner((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    })
+
+    return unsubscribe;
+  }, [dispatch]
+  );
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      dispatch(setCategoriesMap(categoryMap));
+    };
+
+    getCategoriesMap();
+  }, [dispatch]
+  );
 
   return (
     <StrictMode>
       <Routes >
-        <Route path="/" element={ <Navigation /> }> 
-          <Route index element={ <Home /> }/> 
-          <Route path="shop/*" element={ <Shop /> }/> 
-          <Route path="auth" element={ <Authentication /> }/> 
-          <Route path="checkout" element={ <Checkout /> } />
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
         </Route>
       </Routes>
     </StrictMode>
-    
+
   )
 };
 
