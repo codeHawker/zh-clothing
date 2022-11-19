4d053d20ea8b57eb3e2a89296e3870809a9f2866
Zane <zhawken@gmail.com>
2022-11-19 20:24:12 +1300

    added redux-thunk and migrated async calls for category data to be handled with redux

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,22 +1,32 @@
 import { Fragment } from "react";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 import CategoryPreview from "../../components/category-preview/category-preview.component";
+import Spinner from "../../components/spinner/spinner.component";
 
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/category.selector";
 
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
+  console.log("LOADING??", isLoading)
 
   return (
     <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <CategoryPreview
-          title={title}
-          key={title}
-          products={categoriesMap[title]}
-        />
-      ))}
+      { isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(categoriesMap).map((title) => (
+          <CategoryPreview
+            title={title}
+            key={title}
+            products={categoriesMap[title]}
+          />
+        ))
+      )}
     </Fragment>
   );
 };
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { compose, createStore, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
+import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
 import { rootReducer } from './root-reducer'
@@ -9,12 +9,15 @@ import { rootReducer } from './root-reducer'
 const persistConfig = {
     key: 'root',
     storage: storage,
-    blacklist: ['user']
+    whitelist: ['cart']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
+const middleWares = [
+    process.env.NODE_ENV === 'development' && logger,
+    thunk,
+    ].filter(Boolean);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
